f3b6b064b104b216fbb82ad5e41e618944378c05
pmorris50 <patrickmorris532@gmail.com>
2023-01-15 16:26:26 -0700

    homeRoutes.js is hitting

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 router.get('/', (req, res)=>{
     console.log('/ get homepage');
-    res.render('homepage')
+    res.render('homepage');
 
 })
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const authRoutes = require('./authRoutes');
-
+const homeRoutes = require('./homeRoutes')
 router.use('/auth', authRoutes);
-
+router.use('/home', homeRoutes);
 
 module.exports = router;
