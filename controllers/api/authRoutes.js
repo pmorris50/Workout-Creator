6f034ffde5c01af7c4ddd13c3ff311585633b6f7
pmorris50 <patrickmorris532@gmail.com>
2023-01-15 17:01:25 -0700

    I homeRoutes and authRoutes hooked up

--- controllers/api/authRoutes.js
+++ controllers/api/authRoutes.js
@@ -2,16 +2,19 @@ const router = require('express').Router();
 
 //auth login 
 router.get('/login', (req, res) =>{
+    console.log('/ Get Login')
     res.render('login');
 });
 //auth log out
 router.get('/logout', (req, res) => {
+    console.log('Hitting GET /logout')
    //handle with passport
     res.send('loggint out')
 })
 
 //auth with google
 router.get('/google', (req, res) => {
+    console.log('Hitting GET /google')
     //handle with passport
     res.send('loggin in with google')
 })
