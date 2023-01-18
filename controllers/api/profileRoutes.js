f5b3a369cb48aa6c20e7b93da0d120b0395ddc79
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-17 21:44:45 -0500

    adding profileRoutes.js under controller

---
+++ controllers/api/profileRoutes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const { Exercises, Equipment } = require('../../models');
+
+// get all Equipment
+router.get('/', async (req, res)=>{
+    try{
+        const newEquipment = await Equipment.findAll(
+            {
+                include: [{ model:Exercises}]
+            }
+        );
+        res.status(200).json(newEquipment);
+    }catch (err){
+        res.status(500).json(err);
+    }
+});
+
+// get equipment by id 
+router.get ('/:id', async (req, res)=> {
+    try{
+        const newEquipment = await Equipment. findByPk(req.params.id, {
+            include: [{ model: Exercise}]
+        });
+        if (!newEquipment){
+            res.status(404).json({message: 'No Equipment with this ID'});
+            return;
+        };
+        res.status(200).json(newEquipment);
+    }catch (err){
+        res.status (500).json(err);
+    }
+});
+
+//create a new equipment 
+router.post('/', async (req, res)=>{
+    try {
+        const newEquipment = await Equipment.create({
+            id: req.body.id,
+            equipment_name: req.body.equipment_name,
+        });
+        res.status(200).json(newEquipment);
+    }catch (err){
+        res.status(400).json(err);
+    };
+});
+
+
+// delet equipment by ID 
+router.delete('/:id', async (req, res)=> {
+    try{
+        const newEquipment = await  Equipment.destroy({
+            where:{
+                id: req.params.id,
+            }
+        });
+        if (!newEquipment){
+            res.status(404).json({message: 'No equipment with this id'});
+            return;
+        };
+        res.status(200).json(newEquipment);
+    }catch (err){
+        res.status(500).json(err);
+    };
+});
+
+
+module.exports = router;
