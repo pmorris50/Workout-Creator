3be21d6dd0608e1759637a33270398cd08c282de
pmorris50 <patrickmorris532@gmail.com>
2023-01-19 09:41:11 -0700

    Exercises are displaying

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 // const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes')
+const exerciseRoutes = require('./exerciseRoutes')
 // actual sub route so far is /api
 // router.use('/auth', authRoutes);
 router.use('/users', userRoutes)
+router.use('/exercises', exerciseRoutes)
 
 module.exports = router;
